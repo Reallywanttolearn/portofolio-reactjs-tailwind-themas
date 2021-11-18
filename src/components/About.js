cb49e21d92b4f58f7014aecac87b2fd2842c050f
themas <themasfebrianto13@gmail.com>
2021-11-18 14:36:51 +0700

    about done project on progress

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,23 @@
+import logoreact from './react.png'
 import React from "react";
 
 function About() {
     return ( 
         <div>
             <section id="about">
-                <div>
-                    <div>
-                        <h1></h1>
-                        <p></p>
-                        <div>
-                            <a href=""></a>
-                            <a href=""></a>
+                <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+                    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Hallo, I'm Themas
+                                        <br className="hidden lg:inline-block" />I love to build amazing apps. </h1>
+                        <p className="mb-8 leading-relaxed">Web ini di buat untuk mendokumentasikan semua skill dan projek yang sudah saya kuasai atau kerjakan</p>
+                        <div className="flex justify-center">
+                            <a href="#contact" className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">WORK WITH ME</a>
+                            <a href="#projects" className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                            See My Past Work </a>
                         </div>
                     </div>
-                    <div>
-                        <img href=""></img>
+                    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                        <img src={logoreact} className="object-cover object-center rounded" alt="hero"/>
                     </div>
                 </div>
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,28 @@
+import {Codeicon} from "@heroicons/react/solid";
+import react from "react";
+import {project} from "../data";
+
 function Project() {
     return ( 
-        <div></div> );
+        <div>
+            <section>
+                <div>
+                    <div>
+                        <h1> </h1>
+                        <p> </p>
+                    </div>
+                    <div>
+                        <a href="">
+
+                            <div>
+                                <img src="" />
+                            </div>
+                        </a>
+
+                    </div>
+                </div>
+            </section>
+        </div> );
 }
 
 export default Project;
