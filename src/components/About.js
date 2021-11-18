e99b1a05c661d880c7fe651b537f70e7a5bbfa42
themas <themasfebrianto13@gmail.com>
2021-11-18 11:30:00 +0700

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function About() {
+    return ( 
+        <div>
+            <section id="about">
+                <div>
+                    <div>
+                        <h1></h1>
+                        <p></p>
+                        <div>
+                            <a href=""></a>
+                            <a href=""></a>
+                        </div>
+                    </div>
+                    <div>
+                        <img href=""></img>
+                    </div>
+                </div>
+
+            </section>
+        </div> );
+}
+
+export default About;
---
+++ src/components/Testimonials.js
@@ -0,0 +1,6 @@
+function Testimonials() {
+    return ( 
+        <div></div> );
+}
+
+export default Testimonials;
